eae749011e263724b6f707d48c3e461bd7a50674
Saranya-Ramadasan <rsnyaramadasan04@gmail.com>
2026-04-08 15:42:50 +0530

    firstcommit

--- src/AlternateTraversal.java
+++ src/AlternateTraversal.java
@@ -1,8 +1,39 @@
+
+
+import java.util.Scanner;
 public class AlternateTraversal {
     
-        // TODO: Read the matrix
-        // TODO: Traverse the matrix:
-        //       - If row index is even (0, 2...): Print Left -> Right
-        //       - If row index is odd (1, 3...): Print Right -> Left
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        if (!sc.hasNextInt()) return;
+        
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int[][] matrix = new int[m][n];
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        
+        StringBuilder result = new StringBuilder();
+        
+        for (int i = 0; i < m; i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < n; j++) {
+                    result.append(matrix[i][j]).append(" ");
+                }
+            } else {
+                for (int j = n - 1; j >= 0; j--) {
+                    result.append(matrix[i][j]).append(" ");
+                }
+            }
+        }
+        
+        System.out.println(result.toString().trim());
+        sc.close();
+    }
     
 }
